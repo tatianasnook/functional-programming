da91b62090d0f1c4c72f0cd11eec7bee42a3bf03
Tatiana Snook <tatsnook@gmail.com>
2024-11-04 16:37:59 -0600

    3 waves are done

--- activity/functional.py
+++ activity/functional.py
@@ -3,7 +3,7 @@
 # one. (assume no ties)
 
 def shortest_word(words):
-    pass
+    return min(words, key=len)
 
 # SUPER CHALLENGE: use functools.reduce
 # Only try this after completing the rest of the activity
@@ -19,7 +19,7 @@ def shortest_word(words):
 # Hint: remember to convert back to a list!
 
 def even_nums(nums):
-    pass
+    return list(filter(lambda x: x % 2 == 0, nums))
 
 # Wave 3
 # Write a function that takes in a list of numbers and returns a new list
@@ -27,7 +27,7 @@ def even_nums(nums):
 # Hint: remember to convert back to a list!
 
 def squares(nums):
-    pass
+    return list(map(lambda x: x * x, nums))
 
 # Wave 4
 # Write a function that accepts a word, a function, and the name of that 
@@ -35,7 +35,7 @@ def squares(nums):
 # "The result of applying FUNCTION_NAME to WORD is RESULT"
 
 def report(word, function, function_name):
-    pass
+    pass 
 
 # Wave 5
 # Write a function that takes a list of passwords and returns a list of only 
